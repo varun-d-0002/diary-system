705c8dd810df7592d2095ea104122ad3185e44c3
varun-d-0002 <58075804+varun-d-0002@users.noreply.github.com>
2023-03-25 00:48:26 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Previews from './preview.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Previews/>
+      </div>
+  );
+}
+
+export default App;
